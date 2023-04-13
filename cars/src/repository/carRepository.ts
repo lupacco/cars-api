3c29199cdbead1f88fbd9ec1e9cd78b86b0c528b
lupacco <pagottolc@gmail.com>
2023-04-12 21:30:56 -0300

    feat: primsa implementation on repository BUG

--- cars/src/repository/carRepository.ts
+++ cars/src/repository/carRepository.ts
@@ -1,30 +1,45 @@
-import db from "../config/database.js";
+import { prisma } from "../config/database.js";
 
 async function getCars() {
-  const data = await db.query(`SELECT * FROM cars`);
-  return data.rows;
+  const data = await prisma.cars.findMany();
+  return data;
 }
 
 async function getCar(id: number) {
-  const data = await db.query(`SELECT * FROM cars WHERE id = $1`, [id]);
-  return data.rows[0];
+  const data = await prisma.cars.findUnique({
+    where: {
+      id: id,
+    }
+  });
+  return data;
 }
 
 async function getCarWithLicensePlate(licensePlate: string) {
-  const data = await db.query(`SELECT * FROM cars WHERE "licensePlate" = $1`, [licensePlate]);
-  return data.rows[0];
+  const data = await prisma.cars.findUnique({
+    where: {
+      licensePlate : licensePlate,
+    }
+  });
+  return data;
 }
 
 async function createCar(model: string, licensePlate: string, year: number, color: string) {
-  await db.query(
-    `INSERT INTO cars (model, "licensePlate", year, color)
-     VALUES ($1, $2, $3, $4)`,
-    [model, licensePlate, year, color]
-  );
+  await prisma.cars.create({
+    data: {
+      model: model,
+      licensePlate: licensePlate,
+      year: year,
+      color: color
+    }
+  });
 }
 
 async function deleteCar(id: number) {
-  await db.query(`DELETE FROM cars WHERE id = $1`, [id]);
+  await await prisma.cars.delete({
+    where: {
+      id: id,
+    }
+  });
 }
 
 const carRepository = {
